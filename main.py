c2af71e90eece2ad76f9e3d145244081a6e6fe19
Quickmotions <fergusbhaak@gmail.com>
2023-08-12 14:26:46 +0100

    run solar linter

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
-from pprint import pprint
+
+temperature_value_class = '.wr-value--temperature--c'
+time_data_value_class = '.wr-time-slot-secondary__value'
 
 
-def main(loc: str) -> None:
-
+def main(loc: str) -> str:
     loc_dict = defaultdict(str)
 
     with open('locations.dat', 'r') as FH:
         for file in FH.readlines():
             line = file.strip()
             try:
-                id, location = line.split(' ', 1)
-            except ValueError as error:
+                id_num, location = line.split(' ', 1)
+            except ValueError:
                 print(f"Record: {line}")
-                raise Exception("Failed while unpacking. Not enough arguments to supply.")
+                raise ValueError("Failed while unpacking. Not enough arguments to supply.")
 
-            loc_dict[location.lower()] = id
+            loc_dict[location.lower()] = id_num
 
     if loc not in loc_dict:
-        raise Exception(f"Failed to find location ID for {loc}. Supply BBC weather ID in locations.dat file.")
+        raise IndexError(f"Failed to find location ID for {loc}. Supply BBC weather ID in locations.dat file.")
     loc_id = loc_dict[loc.lower()]
 
     r = requests.get(f'https://www.bbc.co.uk/weather/{loc_id}', auth=('user', 'pass'))
     soup = BeautifulSoup(r.text, 'html.parser')
-    highestTemp = soup.select('.wr-value--temperature--c')[0].text
-    lowestTemp = soup.select('.wr-value--temperature--c')[1].text
-    currentTemp = soup.select('.wr-value--temperature--c')[28].text
 
-    humiditiy = soup.select('.wr-time-slot-secondary__value')[0].text
-    pressure = soup.select('.wr-time-slot-secondary__value')[1].text
-    visibility = soup.select('.wr-time-slot-secondary__value')[2].text
+    highest_temp = soup.select(temperature_value_class)[0].text
+    lowest_temp = soup.select(temperature_value_class)[1].text
+    current_temp = soup.select(temperature_value_class)[28].text
+
+    humiditiy = soup.select(time_data_value_class)[0].text
+    pressure = soup.select(time_data_value_class)[1].text
+    visibility = soup.select(time_data_value_class)[2].text
 
     feels_life = soup.select('.wr-time-slot-secondary__feels-like-temperature-value')[0].text
     rain_chance = soup.select('.wr-time-slot-primary__precipitation')[0].text.split("c")[0]
     wind_speed = soup.select('.wr-value--windspeed--mph')[28].text
 
-    return f"High {highestTemp} | Low {lowestTemp} | Current {currentTemp} | Feels Like {feels_life}\nHumidity {humiditiy} | Pressure {pressure} | {visibility} Visibility\nPrecipitation {rain_chance} | Wind Speed {wind_speed}"
+    return f"High {highest_temp} | Low {lowest_temp} | Current {current_temp} | Feels Like {feels_life}\n" \
+           f"Humidity {humiditiy} | Pressure {pressure} | {visibility} Visibility\n" \
+           f"Precipitation {rain_chance} | Wind Speed {wind_speed}"
 
 
 if __name__ == "__main__":
